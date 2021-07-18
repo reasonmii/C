4b3bc0b88fcaf9caa17db83cb6c0db74afc9b87a
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-18 15:37:41 +0900

    Update 15_simpleLinkedList.c

--- 15_simpleLinkedList.c
+++ 15_simpleLinkedList.c
@@ -1,5 +1,5 @@
-// 연결리스트
-// 추가 수정 필요
+// 단순연결리스트 (5~6주차)
+// 추가 수정 필요 -> search, remove + 원형연결리스트
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -36,7 +36,7 @@ void insertFirstNode(linkedList_h* L, char* x) {
 	L->head = newNode;
 }
 
-// 단순 연결리스트 삽입 : 중간 노드로 삽입
+// 단순 연결리스트 삽입 : pre 뒤 중간 노드로 삽입
 void insertMiddleNode(linkedList_h* L, listNode* pre, char* x) {
 	listNode* newNode;
 	newNode = (listNode*)malloc(sizeof(listNode));
@@ -116,7 +116,7 @@ void printList(linkedList_h* L) {
 
 
 // 단순 연결리스트 탐색
-void searchNode(linkedList_h* L, char* x) {
+listNode* searchNode(linkedList_h* L, char* x) {
 	listNode* temp;
 	temp = L->head;
 
@@ -124,7 +124,7 @@ void searchNode(linkedList_h* L, char* x) {
 		if (temp->data == x) return temp;
 		else temp = temp->link;
 	}
-	return temp;
+	return NULL;
 }
 
 
@@ -157,22 +157,30 @@ int main() {
 	printList(L);
 
 	printf("============= 리스트 첫 노드 삽입 ============= \n");
+	insertFirstNode(L, "화");
+	printList(L);
+
 	insertFirstNode(L, "월");
 	printList(L);
 
 	printf("============= 리스트 마지막 노드 삽입 ============= \n");
+	insertLastNode(L, "토");
+	printList(L);
+
 	insertLastNode(L, "일");
 	printList(L);
 
 	printf("============= 리스트 중간 노드 삽입 ============= \n");
 
-	insertMiddleNode(L, pre1, "화");
+	// "화" 뒤에 "수" 삽입
+	insertMiddleNode(L, searchNode(L, "화"), "수");
 	printList(L);
+
+	printf("============= 리스트 노드 삭제 ============= \n");
+	//deleteNode(L, searchNode(L, "화"));
+	//printList(L);
 
 
 	return 0;
 }
-
-
-
 
