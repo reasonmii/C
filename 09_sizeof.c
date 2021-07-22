cdf885a839b867da1eee1f166770ef4d59592b28
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-22 13:15:59 +0900

    Update 09_sizeof.c

--- 09_sizeof.c
+++ 09_sizeof.c
@@ -1,7 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS  // scanf
 #include <stdio.h>
 
-int main(void) {
+int main() {
 
 	int x;
 	double d;
@@ -11,13 +10,15 @@ int main(void) {
 	printf("변수 d의 크기  : %d\n", sizeof(d));   // 8
 	printf("변수 ch의 크기 : %d\n", sizeof(ch));  // 1
 
-	printf("자료형 int의 크기    : %d\n", sizeof(int));     // 4
-	printf("자료형 short의 크기  : %d\n", sizeof(short));   // 2
-	printf("자료형 long의 크기   : %d\n", sizeof(long));    // 4
-	printf("자료형 float의 크기  : %d\n", sizeof(float));   // 4
-	printf("자료형 double의 크기 : %d\n", sizeof(double));  // 8
-	printf("자료형 char의 크기   : %d\n", sizeof(char));    // 1
+	printf("자료형 int의 크기         : %d\n", sizeof(int));         // 4
+	printf("자료형 short의 크기       : %d\n", sizeof(short));       // 2
+	printf("자료형 long의 크기        : %d\n", sizeof(long));        // 4
+	printf("자료형 float의 크기       : %d\n", sizeof(float));       // 4
+	printf("자료형 double의 크기      : %d\n", sizeof(double));      // 8
+	printf("자료형 long double의 크기 : %d\n", sizeof(long double)); // 8
+	printf("자료형 char의 크기        : %d\n", sizeof(char));        // 1
 
+	return 0;
 }
 
 // ======================================================================
