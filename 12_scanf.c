16fd9c6a47bbcc5ad5cf3e55397c990e672a3b6b
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-22 19:49:46 +0900

    Update 12_scanf.c

--- 12_scanf.c
+++ 12_scanf.c
@@ -1,15 +1,15 @@
 /* 
- * Visual studio warns using 'scanf'
- *
- * Method1)
- * Write this code on the top : #define _CRT_SECURE_NO_WARNINGS 
- *
- * Method2)
- * Debug mode
- * Solution Explorer - Right click 'myFirstPrj' - Properties
- * C/C++ - Preprocessor
- * Professor Derinitions : WIN32;_DEBUG;_CONSOLE;%(PreprocessorDefinitions)
- * Change to : WIN32;_DEBUG;_CONSOLE;%(PreprocessorDefinitions);_CRT_SECURE_NO_WARNINGS
+Visual studio warns using 'scanf'
+
+Method1)
+Write this code on the top : #define _CRT_SECURE_NO_WARNINGS 
+
+Method2)
+Debug mode
+Solution Explorer - Right click 'myFirstPrj' - Properties
+C/C++ - Preprocessor
+Professor Derinitions : WIN32;_DEBUG;_CONSOLE;%(PreprocessorDefinitions)
+Change to : WIN32;_DEBUG;_CONSOLE;%(PreprocessorDefinitions);_CRT_SECURE_NO_WARNINGS
 */
 
 // ======================================================================
