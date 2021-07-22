fcd4506e093920e7c140fa4f53385d7dbd2432f3
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-22 09:05:05 +0900

    Update 11_overflowUnderflow.c

--- 11_overflowUnderflow.c
+++ 11_overflowUnderflow.c
@@ -11,7 +11,7 @@ int main() {
 	char buffer[33];
 	_itoa(u_max, buffer, 2);
 
-	// print decimal and binary
+	// print decimal and binary : 오히려 가장 작은 값을 출력함
 	// 10진수 4294967295 + 1 -> 0
 	printf("decimal : %u\n", u_max);
 	// 2진수 11111111111111111111111111111111 + 1 -> 0
@@ -35,7 +35,7 @@ int main() {
 	char buffer[33];
 	_itoa(u_min, buffer, 2);
 
-	// print decimal and binary
+	// print decimal and binary : 오히려 가장 큰 값을 출력함
 	// 10진수 0 - 1 -> 4294967295
 	printf("decimal : %u\n", u_min);
 	// 2진수 0 - 1 -> 11111111111111111111111111111111
