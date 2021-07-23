18fac41e371e89d83c11d2d184435de6d8f15719
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-23 14:30:21 +0900

    Update 26_operator.c

--- 26_operator.c
+++ 26_operator.c
@@ -21,9 +21,124 @@ ex)
 a = 42;
 b = a;              -> a, b 모두 L-value
 c = TWO * (a + b);  -> a+b 값은 R-value
+
+-------------------------------------------
+
+Binary operator 이항 연산자
+피연산자 2개 ex) 3 - 2
+
+Unary operator 단항 연산자
+피연산자 1개
+ex) -16
+
+복합 연산자 : 이항 연산자 & 단항 연산자
+ex) -(12-11)
 */
 
-#define _CRT_SECURE_NO_WARNINGS  // scanf
+// ======================================================================
+
+#include <stdio.h>
+
+int main() {
+
+	int income, salary, bonus;
+
+	// triple assignment
+	// income = (salary = (bonus = 100))
+	income = salary = bonus = 100;
+
+	salary = 100;
+	bonus = 30;
+
+	// income : L-value
+	// salary + bonus : R-value
+	income = salary + bonus;
+
+	// -------------------------------------------
+
+	int a, b;
+	a = -7;
+	b = -a;
+
+	printf("%d %d\n", a, b);  // -7 7
+
+	b = +a; // ★ + does nothing
+	printf("%d %d\n", a, b);  // -7 -7
+
+	return 0;
+}
+
+// ======================================================================
+// 복리 계산하기
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	double start, target, interest;
+
+	printf("Input seed money : ");
+	scanf("%lf", &start);
+
+	printf("Input target money : ");
+	scanf("%lf", &target);
+
+	printf("Input annual interest (%%) : ");
+	scanf("%lf", &interest);
+
+	double fund = start;
+	int year_count = 0;
+
+	while (fund < target) {
+		fund += fund * interest / 100.0;
+		year_count += 1;
+		//year_count++;
+
+		printf("Year %d, fund %f\n", year_count, fund);
+	}
+
+	printf("It takes %d years\n", year_count);
+
+	return 0;
+}
+
+// ======================================================================
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	// 절대 반올림이 아닌 절사로 소수점 없애버림
+	printf("Integer divisions\n");
+	prnitf("%d\n", 14 / 7);  // 2
+	printf("%d\n", 7 / 2);   // 3
+	printf("%d\n", 7 / 3);   // 2
+	printf("%d\n", 7 / 4);   // 1
+	printf("%d\n", 8 / 4);   // 2
+
+	printf("Truncating toward zero (C99)\n");
+	prnitf("%d\n", -7 / 2);  // -3
+	printf("%d\n", -7 / 3);  // -2
+	printf("%d\n", -7 / 4);  // -1
+	printf("%d\n", -8 / 4);  // -2
+
+	printf("Floting divisions\n");
+	printf("%f\n", 9.0 / 4.0);  // 2.250000
+
+	// Note : 4 is integer
+	// CPU는 자료형이 다른 것끼리 계산 불가
+	// compiler가 내부적으로 4를 double 형태인 4.0으로 자동 변환하여 계산
+	// 가능하면, 같은 type으로 연산할 것
+	printf("%f\n", 9.0 / 4);    // 2.250000
+
+	return 0;
+}
+
+// ======================================================================
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void) {
