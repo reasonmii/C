1d92a7be9bd1695c830872bd7cd0869bef242862
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-23 10:18:06 +0900

    Update 43_loop.c

--- 43_loop.c
+++ 43_loop.c
@@ -24,6 +24,22 @@ out:
 
 #include <stdio.h>
 
+int main() {
+
+	int n = 1;
+
+	while (n < 11) {
+		printf("%d\n", n);
+		n = n + 1;
+	}
+
+	return 0;
+}
+
+// ======================================================================
+
+#include <stdio.h>
+
 int main(void) {
 
 	int start = 1;
