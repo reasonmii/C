6aeaa6fe817b12fc377ba459c5916120a28eb97b
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-23 10:16:52 +0900

    Update 43_loop.c

--- 43_loop.c
+++ 43_loop.c
@@ -1,3 +1,27 @@
+// ※ goto문
+// 옛날 loop 방식인데, 코드가 꼬이고,
+// 에러가 발생할 확률이 많아서 요즘은 사용X -> 단종된 기술
+
+#include <stdio.h>
+
+int main() {
+
+	int n = 1;
+
+label:
+	printf("%d\n", n);
+	n = n + 1;
+
+	if (n == 10) goto out;
+	goto label;
+
+out:
+
+	return 0;
+}
+
+// ======================================================================
+
 #include <stdio.h>
 
 int main(void) {
