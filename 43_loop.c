081518fce24fbec9197f051a18d14eb14025f597
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-07-23 14:30:46 +0900

    Update 43_loop.c

--- 43_loop.c
+++ 43_loop.c
@@ -91,6 +91,38 @@ int main(void) {
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int main() {
+
+	int seconds = 0, minutes = 0, hours = 0;
+
+	printf("Input seconds : ");
+	scanf("%d", &seconds);
+
+	while (seconds >= 0) {
+
+		minutes = seconds / 60;
+		seconds %= 60;
+
+		hours = minutes / 60;
+		minutes %= 60;
+
+		// print result
+		printf("%d hours, %d minutes, %d seconds\n", hours, minutes, seconds);
+
+		printf("Input seconds : ");
+		scanf("%d", &seconds);
+	}
+
+	printf("Good bye\n");
+
+	return 0;
+}
+
+// ======================================================================
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
 int main(void) {
 
 	int start = 1;
