87044f9799631d567cd6f9705b4ba10cef293bc6
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-02 11:29:18 +0900

    Update 45_recursion.c

--- 45_recursion.c
+++ 45_recursion.c
@@ -40,3 +40,104 @@ void my_func(int n) {
 	printf("Level %d, address of variable n = %p\n", n, &n);
 }
 
+// ======================================================================
+// 팩토리얼 : loop vs. recursion
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+long loop_factorial(int n);
+long recursive_factorial(int n);
+
+int main() {
+
+	int num = 5;
+	printf("%d\n", loop_factorial(num));
+	printf("%d\n", recursive_factorial(num));
+
+	return 0;
+}
+
+long loop_factorial(int n) {
+
+	int rst;
+	for (rst = 1; n > 1; n--)
+		rst *= n;
+
+	return rst;
+}
+
+long recursive_factorial(int n) {
+
+	if (n > 0)
+		// tail/end recursion
+		// return 바로 뒤에 recursive call을 하는 것
+		return n * recursive_factorial(n - 1);
+	else
+		return 1;
+
+}
+
+// ======================================================================
+// 팩토리얼 : 숫자 입력받기
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+long factorial(int n);     // 함수선언
+
+int main(void) {
+	int n;
+	printf("정수 입력 : ");
+	scanf("%d", &n);
+	printf("%d! = ", n);  // ex)  4! = 4 * 3 * 2 * 1
+	factorial(n);
+
+	return 0;
+}
+
+long factorial(int n) {
+	printf("%d", n);
+	if (n == 1)
+		return 1;
+	else {
+		printf(" * ");
+		return n * factorial(n - 1);
+	}
+}
+
+// ======================================================================
+/*
+하노이의 탑
+A에서 원반 1를 C로 옮김
+*****A에서 원반 2를 B로 옮김
+#####C에서 원반 1를 B로 옮김
+=====*****A에서 원반 3를 C로 옮김
+#####B에서 원반 1를 A로 옮김
+*****B에서 원반 2를 C로 옮김
+#####A에서 원반 1를 C로 옮김
+==========
+*/
+
+#include <stdio.h>
+
+void hanoi(int n, int start, int work, int target);
+
+void main() {
+	hanoi(3, 'A', 'B', 'C');
+	getchar();
+}
+
+void hanoi(int n, int start, int work, int target) {
+	if (n == 1)
+		printf("%c에서 원반 %d를 %c로 옮김 \n", start, n, target);
+	else {
+		hanoi(n - 1, start, target, work);
+		printf("*****");
+		printf("%c에서 원반 %d를 %c로 옮김 \n", start, n, target);
+		printf("#####");
+		hanoi(n - 1, work, start, target);
+		printf("=====");
+	}
+}
+
