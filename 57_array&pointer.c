c43ad54c8a7d9ab25d410ffb0b3d1c007d1478fe
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-03 11:34:13 +0900

    Update 57_array&pointer.c

--- 57_array&pointer.c
+++ 57_array&pointer.c
@@ -221,3 +221,89 @@ int main() {
 
 	return 0;
 }
+
+// ======================================================================
+/*
+In higher dimensions,
+you may omit the value in the left-most bracket
+고차원 배열에서 보통 가장 왼쪽 값은 생략 []
+다른 값은 입력해야 함
+
+예시)
+int my_function(int ar[][3][5][8], int rows);
+int my_function(int (*ar)[][3][5][8], int rows);
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define ROWS 3
+#define COLS 4
+
+// 배열의 가장 왼쪽 [] ROWS자리는 Compiler가 빈칸으로 인식함
+// 따라서 생략해도 상관 없음
+int sum2d_1(int ar[ROWS][COLS]);
+
+
+int sum2d_2(int ar[][COLS], int row);
+// 방법2)
+// int sum2d_2(int [][COLS], int row);
+// 방법3)
+// ar is a SINGLE pointer to the array of COLS ints
+// COLS 개수만큼의 배열을 갖는 배열에 대한 단일 포인터
+// int sum2d_2(int (*ar)[COLS], int row);
+
+// 동적할당에서 사용하는 방법
+// 실제 다차원 배열에서 많이 사용
+int sum2d_3(int *ar, int row, int col);
+// 방법2) 변수 이름 생략 가능
+// int sum2d_3(int*, int, int);
+
+int main() {
+
+	int data[ROWS][COLS] = {
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 0, 1, 2}
+	};
+
+	// 배열의 원소 접근하기
+	printf("%d\n", data[2][3]);
+
+	// data[2][3]
+	// 배열의 첫 번째 원소 포인터를 활용해
+	// 포인터 연산으로 배열 접근하기
+	int* ptr = &data[0][0];
+	printf("%d\n", *(ptr + 3 + COLS * 2));
+
+	printf("Sum of all elements = %d\n", sum2d_1(data));
+	printf("Sum of all elements = %d\n", sum2d_2(data, ROWS));
+	printf("Sum of all elements = %d\n", sum2d_3(&data[0][0], ROWS, COLS));
+
+
+	return 0;
+}
+
+int sum2d_1(int ar[ROWS][COLS]) {
+	int r, c, tot = 0;
+	for (r = 0; r < ROWS; r++)
+		for (c = 0; c < COLS; c++)
+			tot += ar[r][c];
+	return tot;
+}
+
+int sum2d_2(int ar[][COLS], int row) {
+	int r, c, tot = 0;
+	for (r = 0; r < row; r++)
+		for (c = 0; c < COLS; c++)
+			tot += ar[r][c];
+	return tot;
+}
+
+int sum2d_3(int* ar, int row, int col) {
+	int r, c, tot = 0;
+	for (r = 0; r < row; r++)
+		for (c = 0; c < col; c++)
+			tot += *(ar + c + col * r);  // ar[c + col * r]
+	return tot;
+}
