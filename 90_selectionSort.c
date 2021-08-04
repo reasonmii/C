f0cc54c536ac909fcb0b588836a8d4396d502f04
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-04 23:44:13 +0900

    Update 90_selectionSort.c

--- 90_selectionSort.c
+++ 90_selectionSort.c
@@ -63,3 +63,68 @@ void selectionSort(int arr[], int n) {
 		swap(&arr[min_idx], &arr[i]);
 	}
 }
+
+// ======================================================================
+/*
+문자열의 포인터 정렬하기
+Cherry
+AppleBee
+Pineapple
+Apple
+Orange
+
+Apple
+AppleBee
+Cherry
+Orange
+Pineapple
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>   // strcmp()
+
+void swap(char** xp, char** yp);
+void printStringArray(char* arr[], int size);
+void selectionSort(char* arr[], int n);
+
+int main() {
+
+	char* arr[] = { "Cherry", "AppleBee", "Pineapple", "Apple", "Orange" };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	printStringArray(arr, n);
+
+	// ascending order
+	selectionSort(arr, n);
+
+	printStringArray(arr, n);
+
+	return 0;
+}
+
+void swap(char** xp, char** yp) {
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void printStringArray(char* arr[], int size) {
+	for (int i = 0; i < size; i++)
+		puts(arr[i]);
+	printf("\n");
+}
+
+void selectionSort(char* arr[], int n) {
+
+	int i, j, min_idx;
+
+	for (i = 0; i < n - 1; i++) {
+		min_idx = i;
+		for (j = i + 1; j < n; j++) {
+			if (strcmp(arr[j], arr[min_idx]) < 0)
+				min_idx = j;
+		}
+		swap(&arr[min_idx], &arr[i]);
+	}
+}
