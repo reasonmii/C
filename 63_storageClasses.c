71ed14ecc357e4ab5cba98a81b5ab0bb2d7dd263
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-05 17:09:53 +0900

    Update 63_storageClasses.c

--- 63_storageClasses.c
+++ 63_storageClasses.c
@@ -1,4 +1,15 @@
 /*
+5 Storage Classes
+1) Automatic
+2) Register
+3) Static with internal linkage
+4) Static with external linkage
+5) Static with no linkage
+*/
+
+// ======================================================================
+
+/*
 Automatic storage class : 지역변수
 - Automatic storage duration, block scope, no linkage
 - Any variable declared in a block or function header
@@ -88,79 +99,74 @@ int main() {
 
 // ======================================================================
 /*
-Static with no linkage 블록 영역의 정적 변수
-Scope : 블록 안
-선언 방법 : 블록 안에서 static 키워드 사용
+Static with internal Linkage
+정적 변수의 내부 연결
+- File scope, external linkage, static storage uration
+- Internal storage class
 
-※ 함수 parameter에 static 사용
-ex) int func(static int i) {}
--> Warning (Error in GCC)
-함수가 실행될 때는 새로운 stack이 배정이 됨
-즉, 함수 parameter 변수는 함수가 실행될 때 메모리를 배정받음
-그런데 변수 앞에 static을 붙인다는 건,
-paramter 변수를 static한 공간에 억지로 넣어 메모리를 차지하고 있겠다는 의미
-이렇게 static이 필요한 변수면 전역변수를 쓰거나 다른 방법을 해야 함
-
-★ 매우 안 좋은 프로그래밍
-지역변수의 포인터를 반환하는 함수인데 ct는 이 block이 끝나면 사라짐
-> Warning 발생 : returning address of local variable or temporary
-int* count() {
-	int ct = 0;
-	printf("count = %d %lld\n", ct, (long long)&ct);
-	ct++;
-
-	return &ct;
-}
-
-아래처럼 static 변수인 경우 고정된 메모리 자리를 가지고 있기 때문에,
-권장하지 않지만 어쩔 수 없는 경우에는 사용해도 됨
-이런 경우에는 차라리 전역변수 쓸 것
-int* static_count() {
-	static int ct = 0;
-	printf("static count = %d %lld\n", ct, (long long)&ct);
-	ct++;
-
-	return &ct;
-}
+Scope : 파일 안
+선언 방법 : 모든 함수들 밖에서 static 키워드 사용
+연결 상태 : translation unit(번역 단위)의 내부에서만 사용
+           = 같은 파일 안에서만 연결 가능
 */
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void count() {
-	int ct = 0;
-	printf("count = %d %lld\n", ct, (long long)&ct);
-	ct++;
-}
+// Defining declaration
+// int g_int = 123;
+// ★ static 붙이면 다른 파일과 linkage 막아버림
+static int g_int = 123;
 
-void static_count() {
-	// ★ initialized only once!
-	static int ct = 0;
-	printf("static count = %d %lld\n", ct, (long long)&ct);
-	ct++;
-}
-
-void counter_caller() {
-	count();
-}
-
-void static_counter_caller() {
-	static_count();
-}
+void func();
+void func_second();
+void func_third();
 
 int main() {
 
-	count();           // count = 0 11532536
-	count();           // count = 0 11532536
-	counter_caller();  // count = 0 11532324 -> 메모리가 계속 새롭게 배정됨
-
-	static_count();           // static count = 0 14459192
-	static_count();           // ★ static count = 1 14459192
-	static_counter_caller();  // ★ static count = 2 14459192 -> 주소 변화X
+	func();        // 124 00E5A000
+	func_second(); // 1 005CA584
+	func_third();  // 2 005CA584 -> ★ second.c 파일 g_int를 외부변수로 사용
 
 	return 0;
 }
 
+void func() {
+	// optional
+	extern int g_int;
+	g_int += 1;
+	printf("g_int in func_second() %d %p\n", g_int, &g_int);
+}
+
+// -------------------------------------------
+// Solution Explorer 창에서 현재 프로젝트 우클릭
+// C파일 생성 ex) second.c
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// referencing declaration
+int g_int;
+
+void func_second(void) {
+	g_int += 1;
+	printf("g_int in func_second() %d %p\n", g_int, &g_int);
+}
+
+// -------------------------------------------
+// Solution Explorer 창에서 현재 프로젝트 우클릭
+// C파일 생성 ex) third.c
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// referencing declaration
+extern int g_int;
+
+void func_third(void) {
+	g_int += 1;
+	printf("g_int in func_second() %d %p\n", g_int, &g_int);
+}
 
 // ======================================================================
 /*
@@ -274,13 +280,76 @@ void func_sec() {
 
 // ======================================================================
 /*
-Internal Linkage
-정적 변수의 내부 연결
+Static with no linkage 블록 영역의 정적 변수
+Scope : 블록 안
+선언 방법 : 블록 안에서 static 키워드 사용
 
+※ 함수 parameter에 static 사용
+ex) int func(static int i) {}
+-> Warning (Error in GCC)
+함수가 실행될 때는 새로운 stack이 배정이 됨
+즉, 함수 parameter 변수는 함수가 실행될 때 메모리를 배정받음
+그런데 변수 앞에 static을 붙인다는 건,
+paramter 변수를 static한 공간에 억지로 넣어 메모리를 차지하고 있겠다는 의미
+이렇게 static이 필요한 변수면 전역변수를 쓰거나 다른 방법을 해야 함
+
+★ 매우 안 좋은 프로그래밍
+지역변수의 포인터를 반환하는 함수인데 ct는 이 block이 끝나면 사라짐
+> Warning 발생 : returning address of local variable or temporary
+int* count() {
+	int ct = 0;
+	printf("count = %d %lld\n", ct, (long long)&ct);
+	ct++;
+
+	return &ct;
+}
+
+아래처럼 static 변수인 경우 고정된 메모리 자리를 가지고 있기 때문에,
+권장하지 않지만 어쩔 수 없는 경우에는 사용해도 됨
+이런 경우에는 차라리 전역변수 쓸 것
+int* static_count() {
+	static int ct = 0;
+	printf("static count = %d %lld\n", ct, (long long)&ct);
+	ct++;
+
+	return &ct;
+}
 */
 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+void count() {
+	int ct = 0;
+	printf("count = %d %lld\n", ct, (long long)&ct);
+	ct++;
+}
 
+void static_count() {
+	// ★ initialized only once!
+	static int ct = 0;
+	printf("static count = %d %lld\n", ct, (long long)&ct);
+	ct++;
+}
 
+void counter_caller() {
+	count();
+}
 
+void static_counter_caller() {
+	static_count();
+}
+
+int main() {
+
+	count();           // count = 0 11532536
+	count();           // count = 0 11532536
+	counter_caller();  // count = 0 11532324 -> 메모리가 계속 새롭게 배정됨
+
+	static_count();           // static count = 0 14459192
+	static_count();           // ★ static count = 1 14459192
+	static_counter_caller();  // ★ static count = 2 14459192 -> 주소 변화X
+
+	return 0;
+}
 
