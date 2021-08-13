4807e1cfc33cff31cc6b7ce16a5dfb50f51018c8
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-13 16:53:15 +0900

    Create 53_abstractDataTypes.c

---
+++ 53_abstractDataTypes.c
@@ -0,0 +1,37 @@
+/*
+※ book 'Fundamentals of Data Structures in C++
+         2nd Edition by Horowitz, Ellis'
+
+Abstract Data Types 추상 자료형
+- A data type is a collection of objects
+  and a set of operations that act on those objects
+- 자료형의 명세서
+- 객체와 연산의 사양(specification)이 객체의 표현과 연산의
+  구현으로부터 분리되도록 구성된 자료형
+- 어떠한 프로그래밍 언어를 사용해서 어떻게 구현했는지를 제외
+  ★ 추상 자료형의 실제구현 : 자료구조 사용
+
+Encapsulation 캡슐화
+- Data Encapsulation or Information Hiding is the concealing
+  of the implementation details of a data object from the outside world
+
+Abstraction 추상화
+- Data Abstraction is the separation between the specification
+  of a data object and its implementation
+- 데이터가 어떻게 사용되는지 명세(specification)만 알려주고,
+  이를 어떻게 구현(implementation)했는지는 모르게 두는 것
+- ex) TV가 돌아가는 원리를 사용자는 알지 못함
+
+캡슐화와 추상화의 장점
+- 이해하기 쉽고 수정하기 쉬움
+- 재사용이 편리 (명세가 명확하기 때문)
+- 프로그래밍의 효율 up
+
+Interface 인터페이스
+- 프로그래밍 언어로 쓰여진 자료형의 명세서
+- 추상 자료형과 실제 구현 사이의 다리 역할
+*/
+
+
+
+
