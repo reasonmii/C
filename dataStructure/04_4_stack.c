f513185a6ea298f63c1b82ab66a58c31358aaf94
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-08-20 10:17:44 +0900

    Create 04_4_stack.c

---
+++ dataStructure/04_4_stack.c
@@ -0,0 +1,47 @@
+#include "stack.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// top은 보통 배열의 index로 사용됨 
+void Initialize(Stack* stack) {
+	stack->top = -1;
+	// allocate memory if dynamic array is used
+}
+
+bool IsFull(const Stack* stack) {
+	if (stack->top >= MAX_STACK_SIZE - 1)
+		return true;
+	else
+		return false;
+}
+
+bool IsEmpty(const Stack* stack) {
+	if (stack->top == -1)
+		return true;
+	else
+		return false;
+}
+
+void Push(Stack* stack, element iem) {
+
+	if (IsFull(stack) == true)
+		printf("Stack is full. Cannot add.\n");
+	else {
+		//memcpy(&stack->items[++stack->top], &item, sizeof(item));
+		stack->items[++stack->top] = item;
+	}
+
+}
+
+element Pop(Stack* stack) {
+
+	if (isEmpty(stack) == true) {
+		printf("Stack is empty. Cannot remove.\n");
+
+		// -1을 가지고 있는 element를 반환
+		element temp = { -1 }; // Place holder, dummy : fake data  
+		return temp;
+	}
+	return stack->item[stack->top--];
+}
